6e6d4538da1598392c1b5c4042229216bb729214
Gojo <vishalvc16vc@gmail.com>
2026-04-04 16:10:06 +0530

    Update stats.js

--- stats.js
+++ stats.js
@@ -1,2 +1,5 @@
-const REPO_VERSION = "1.0.0";
-const REPO_NAME = "BASED-FLAPPY";
+// BASED-FLAPPY - Repository stats
+const APP_VERSION = "1.0.0";
+const NETWORK = "Base Mainnet";
+const ENTRY_FEE = "0.00005 ETH";
+const PRIZE_SPLIT = { first: 50, second: 40, fee: 10 };
