49246c5fb05e09a845314aa67d2d4a2ccc44ead3
Gojo <vishalvc16vc@gmail.com>
2026-04-04 16:11:06 +0530

    Update types.ts

--- types.ts
+++ types.ts
@@ -1,1 +1,11 @@
-export const VERSION = "1.0.0";
+export interface GameConfig {
+  version: string;
+  network: string;
+  entryFee: string;
+}
+
+export const config: GameConfig = {
+  version: "1.0.0",
+  network: "Base Mainnet",
+  entryFee: "0.00005 ETH"
+};
