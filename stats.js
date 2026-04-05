c1084cc0fb7b15588ea030e611bb22be3fce7dad
Gojo <vishalvc16vc@gmail.com>
2026-04-05 12:40:45 +0530

    Update next.config.js with new configuration

--- stats.js
+++
@@ -1,5 +0,0 @@
-// BASED-FLAPPY - Repository stats
-const APP_VERSION = "1.0.0";
-const NETWORK = "Base Mainnet";
-const ENTRY_FEE = "0.00005 ETH";
-const PRIZE_SPLIT = { first: 50, second: 40, fee: 10 };
